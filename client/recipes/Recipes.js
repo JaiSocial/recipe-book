c65d41225ce0599ee1dd494e95d911e25aa74116
Jaideep Sundaram <jsocial2020@gmail.com>
2017-07-29 08:16:46 -0400

    initial check-in

---
+++ client/recipes/Recipes.js
@@ -0,0 +1,23 @@
+// Meteor.subscribe('recipes');
+
+Template.Recipes.onCreated(function (){
+
+	var self = this;
+	self.autorun(function(){
+		self.subscribe('recipes');
+	})
+});
+
+// console.log(Meteor.settings.public.ga.account);
+Template.Recipes.helpers({
+	recipes: () => {
+		return Recipes.find({});
+	}
+})
+
+
+Template.Recipes.events({
+	'click .new-recipe': function (){
+		Session.set('newRecipe', true);
+	}
+});
